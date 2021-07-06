8a0121576c9ba93165b90f7f886a13d9e9b0bc22
Claus Kovacs <claus@kovacs.cf>
2021-07-06 23:19:54 +0100

    unit test: testing (fail)

--- test/test_runtest.py
+++ test/test_runtest.py
@@ -6,7 +6,7 @@ import unittest
 class Unit_test_class(unittest.TestCase):
 
 	def test_func1(self):
-		self.assertEqual(1, 1)
+		self.assertEqual(1, 0)
 
 if __name__ == '__main__':
 	unittest.main()
